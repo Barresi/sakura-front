e09f3e31363d6adfb980d76d665ce50ff31b353b
Dunissimo <SuperDen214@yandex.ru>
2023-08-20 15:21:41 +0400

    install and configure libs

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,9 @@ import { FC } from "react";
 const App: FC = () => {
   return (
     <div>
-      <h1>Hello world! Sakura's here!</h1>
+      <h1 className="text-center text-lg mt-8 text-red-500">
+        Hello world! Sakura's here!
+      </h1>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
-import App from "./components/app";
+import "./main.css";
+import App from "./components/app/app";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+// Example of new version of react-router-dom
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
