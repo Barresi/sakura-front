ee1d918bb33f32fc03b534c26c24a1b18950d0c1
Dunissimo <SuperDen214@yandex.ru>
2023-08-20 21:09:44 +0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,18 @@ import ReactDOM from "react-dom/client";
 
 import "./main.css";
 import App from "./components/app/app";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-
-// Example of new version of react-router-dom
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
