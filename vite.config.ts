735186f93b863c91b93d912faf3397a6dd756db8
Dunissimo <SuperDen214@yandex.ru>
2023-08-29 14:13:08 +0400

    add more paths, install clsx

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@ui": path.resolve(__dirname, "./src/ui"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
 });
