a9af4e70096ed3a8d749be26ac3c8a307fbce73d
Dunissimo <SuperDen214@yandex.ru>
2023-09-23 23:18:26 +0400

    fix: content' position in mobile nav

--- src/components/mobile-nav/mobile-nav.tsx
+++ src/components/mobile-nav/mobile-nav.tsx
@@ -3,12 +3,10 @@ import NavButton from "../ui/button/nav-button/nav-button";
 
 const MobileNav: FC = () => {
   return (
-    <div className="w-full fixed bottom-0 left-0 flex items-center gap-[5px] lg:gap-[10px] bg-navButton text-navButton-foreground py-[10px] px-[10px] lg:px-[20px] rounded-tl-[10px] rounded-tr-[10px] z-[10000] bg-background">
+    <div className="w-full fixed bottom-0 left-0 flex items-center justify-between gap-[5px] lg:gap-[10px] bg-navButton text-navButton-foreground py-[10px] px-[10px] lg:px-[20px] rounded-tl-[10px] rounded-tr-[10px] z-[10000] bg-background">
       <NavButton
         className="w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]"
-        iconPos="left"
         icon="user"
-        variant="text"
         to="profile"
       >
         Главная
@@ -16,9 +14,7 @@ const MobileNav: FC = () => {
 
       <NavButton
         className="w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]"
-        iconPos="left"
         icon="news"
-        variant="text"
         to="feed"
       >
         Новости
@@ -26,9 +22,7 @@ const MobileNav: FC = () => {
 
       <NavButton
         className="w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]"
-        iconPos="left"
         icon="message"
-        variant="text"
         to="messenger"
       >
         Мессенджер
@@ -36,9 +30,7 @@ const MobileNav: FC = () => {
 
       <NavButton
         className="w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]"
-        iconPos="left"
         icon="friends"
-        variant="text"
         to="friends"
       >
         Друзья
@@ -46,9 +38,7 @@ const MobileNav: FC = () => {
 
       <NavButton
         className="w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]"
-        iconPos="left"
         icon="photos"
-        variant="text"
         to="photos"
       >
         Фотографии
