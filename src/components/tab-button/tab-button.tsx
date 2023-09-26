7920f7d6f81ff3032cced56502d9a894a19d1c36
Dunissimo <SuperDen214@yandex.ru>
2023-09-26 16:59:26 +0400

    ref: tabs in friends

--- src/components/tab-button/tab-button.tsx
+++ src/components/tab-button/tab-button.tsx
@@ -1,57 +1,16 @@
-import { FC, ReactNode, useState } from "react";
+import { FC, ReactNode } from "react";
 import Button from "@ui/button/button";
 import { Badge } from "@ui/badge/badge";
-
-type obj = {
-  text: string;
-  type: string;
-  badge?: number;
-};
-
-interface TabContainerProps {
-  buttons: obj[];
-  className?: string;
-}
-
-export const TabContainer: FC<TabContainerProps> = ({ buttons, className }) => {
-  const [active, setActive] = useState(buttons[0].type);
-
-  const clickHandler = (type: string) => {
-    setActive(type);
-  };
-
-  return (
-    <div className={className}>
-      {buttons.map(({ text, badge, type }) => (
-        <TabButton
-          badge={badge}
-          onClick={() => clickHandler(type)}
-          active={type === active}
-        >
-          {text}
-        </TabButton>
-      ))}
-    </div>
-  );
-};
 
 interface TabButtonProps {
   children: ReactNode;
   badge?: number;
-  active?: boolean;
-  onClick?: () => void;
 }
 
-const TabButton: FC<TabButtonProps> = ({ children, active, onClick, badge }) => {
-  console.log(badge);
-
+const TabButton: FC<TabButtonProps> = ({ children, badge }) => {
   return (
     <div>
-      <Button
-        className={`w-full flex justify-start ${active && "bg-text"}`}
-        variant="text"
-        onClick={onClick}
-      >
+      <Button className={`w-full flex justify-start`} variant="text">
         {children}
         {badge && <Badge>{badge}</Badge>}
       </Button>
--- src/pages/friends/friends.tsx
+++ src/pages/friends/friends.tsx
@@ -1,10 +1,10 @@
 import FriendCard from "@src/components/ui/card/friend-card/friend-card";
 import Search from "@src/components/ui/form/search/search";
 import { FC } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui/tabs/tabs";
+import TabButton from "@components/tab-button/tab-button";
 
 import photo from "@assets/photo.svg";
-import NavButton from "@src/components/ui/button/nav-button/nav-button";
-import { TabContainer } from "@src/components/tab-button/tab-button";
 
 const friends = [
   {
@@ -34,48 +34,58 @@ const friends = [
   },
 ];
 
+const buttons = [
+  {
+    text: "Мои друзья",
+    type: "friends",
+  },
+  {
+    text: "Все пользователи",
+    type: "all",
+  },
+  {
+    text: "Заявки в друзья",
+    type: "requests",
+    badge: 5,
+  },
+  {
+    text: "Отправленные заявки",
+    type: "sended",
+  },
+];
+
 const FriendsPage: FC = () => {
   return (
-    <div className="w-full flex flex-col xl:flex-row-reverse justify-between gap-[20px] lg:gap-[30px] mt-[20px] xl:mt-[30px] mb-[20px]">
-      <div className="w-full xl:w-1/3 max-h-[22%] bg-background rounded-[10px] p-[30px]">
-        <TabContainer
-          className="flex flex-row xl:flex-col justify-between xl:justify-start"
-          buttons={[
-            {
-              text: "Мои друзья",
-              type: "friends",
-            },
-            {
-              text: "Все пользователи",
-              type: "all",
-            },
-            {
-              text: "Заявки в друзья",
-              type: "requests",
-              badge: 5,
-            },
-            {
-              text: "Отправленные заявки",
-              type: "sended",
-            },
-          ]}
-        />
-      </div>
+    <Tabs
+      defaultValue={buttons[0].type}
+      className="w-full flex flex-col xl:flex-row-reverse justify-between gap-[20px] lg:gap-[30px] mt-[20px] xl:mt-[30px] mb-[20px]"
+    >
+      <TabsList className="w-full xl:w-1/3 h-auto max-h-[200px] flex flex-col bg-background rounded-[10px] p-[30px]">
+        {buttons.map(({ text, type, badge }, index) => (
+          <TabsTrigger value={type} key={index}>
+            <TabButton badge={badge}>{text}</TabButton>
+          </TabsTrigger>
+        ))}
+      </TabsList>
 
       <div className="list w-full xl:w-2/3 bg-background rounded-[10px] p-[30px]">
-        <h2 className="font-medium text-[24px] leading-[32px]">Мои друзья</h2>
+        {buttons.map(({ text, type }, index) => (
+          <TabsContent value={type} key={index}>
+            <h2 className="font-medium text-[24px] leading-[32px]">{text}</h2>
 
-        <div className="mt-[20px]">
-          <Search />
-        </div>
+            <div className="mt-[20px]">
+              <Search />
+            </div>
 
-        <div className="flex flex-col gap-[20px]">
-          {friends.map(({ img, imgFallback, name }) => (
-            <FriendCard img={img} imgFallback={imgFallback} name={name} />
-          ))}
-        </div>
+            <div className="flex flex-col gap-[20px]">
+              {friends.map(({ img, imgFallback, name }) => (
+                <FriendCard img={img} imgFallback={imgFallback} name={name} />
+              ))}
+            </div>
+          </TabsContent>
+        ))}
       </div>
-    </div>
+    </Tabs>
   );
 };
 
