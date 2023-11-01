81f731492c2d3bb0606af5426b5f66e065b0cf0d
Barresi <Arhcer.php@gmail.com>
2023-11-02 01:04:37 +0700

    fix: utils.ts file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,18 +2,20 @@ import { type ClassValue, clsx } from 'clsx'
 import { useState, useEffect } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-export function cn (...inputs: ClassValue[]) {
+export function cn (...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
 
-export function useWindowSize (maxWidth: string | number) {
+export function useWindowSize (maxWidth: string | number): boolean {
   const [width, setWidth] = useState(window.innerWidth)
 
+  const handleResize = (event: Event): void => {
+    setWidth((event.target as Window).innerWidth)
+  }
+
   useEffect(() => {
-    const handleResize = (event: Event) => {
-      setWidth((event.target as Window).innerWidth)
-    }
     window.addEventListener('resize', handleResize)
+
     return () => {
       window.removeEventListener('resize', handleResize)
     }
