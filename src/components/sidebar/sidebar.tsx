e36a101b97e4ad4ffab575a9f0a415eec9581510
Barresi <Arhcer.php@gmail.com>
2023-11-09 02:19:15 +0700

    fix: delete badge in sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,14 +2,12 @@ import { type FC } from 'react'
 import Logo from '../ui/logo/logo'
 import NavButton from '../ui/button/nav-button/nav-button'
 import SettingButton from '@src/components/ui/button/setting-button/setting-button'
-import { useAppDispatch, useAppSelector } from '@src/hooks/store-hooks'
+import { useAppDispatch } from '@src/hooks/store-hooks'
 import { logoutThunk } from '@src/store/reducers/profileInfo/async-thunks'
 import { useTheme } from '@src/hooks/useTheme'
-import { selectReceived } from '@src/store/reducers/friends/selectors'
 
 const Sidebar: FC = () => {
   const dispatch = useAppDispatch()
-  const received = useAppSelector(selectReceived)
   const { toggleTheme } = useTheme()
   const logoutHandler = async (): Promise<void> => {
     await dispatch(logoutThunk())
@@ -40,7 +38,6 @@ const Sidebar: FC = () => {
             className='w-full justify-start gap-[10px]'
             icon='friends'
             to='friends'
-            badge={received.length}
           >
             Друзья
           </NavButton>
