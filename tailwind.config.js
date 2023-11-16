4bc6d88da741daa27e92ac363f5e075d3fa05860
Barresi <Arhcer.php@gmail.com>
2023-11-17 00:55:43 +0700

    fix: change tailwind config (delete extend)

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,36 +20,34 @@ module.exports = {
       center: true,
       padding: '2rem'
     },
-    extend: {
-      colors: {
-        white: '#fff',
-        darkWhite: '#f2f2f2',
-        ghostlyWhite: '#f8fafc',
-        smokyWhite: ' #f1f5f9',
-        cadet: '#55677c',
-        cadetBlue: '#adb5bd',
-        lightGray: '#d6d6d6',
-        darkLightGray: '#929292',
-        twitter: '#20b4ee',
-        myBlue: '#4791ff',
-        myRed: '#d22828',
-        redHover: '#c32222',
-        darkGray: '#adb5bd',
-        signalBlack: '#292929',
-        brownBlack: '#202028',
-        black: '#000',
-        grayBlue: '#23232f',
+    colors: {
+      white: '#fff',
+      darkWhite: '#f2f2f2',
+      ghostlyWhite: '#f8fafc',
+      smokyWhite: ' #f1f5f9',
+      cadet: '#55677c',
+      cadetBlue: '#adb5bd',
+      lightGray: '#d6d6d6',
+      darkLightGray: '#929292',
+      twitter: '#20b4ee',
+      myBlue: '#4791ff',
+      myRed: '#d22828',
+      redHover: '#c32222',
+      darkGray: '#adb5bd',
+      signalBlack: '#292929',
+      brownBlack: '#202028',
+      black: '#000',
+      grayBlue: '#23232f',
 
-        body: '#eeeff1',
-        bodyDark: '#191924',
-        border: 'hsl(214.3, 31.8%, 91.4%)',
-        borderDark: 'hsl(217, 0.2, 32)'
-      },
-      borderRadius: {
-        lg: '0.5rem',
-        md: 'calc(0.5rem - 2px)',
-        sm: 'calc(0.5rem - 4px)'
-      }
+      body: '#eeeff1',
+      bodyDark: '#191924',
+      border: 'hsl(214.3, 31.8%, 91.4%)',
+      borderDark: 'hsl(217, 0.2, 32)'
+    },
+    borderRadius: {
+      lg: '0.5rem',
+      md: 'calc(0.5rem - 2px)',
+      sm: 'calc(0.5rem - 4px)'
     }
   },
   plugins: [require('tailwindcss-animate')]
