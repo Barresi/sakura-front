4cc23e96e583435de834571118f83a19a69869b7
Dunissimo <SuperDen214@yandex.ru>
2023-11-27 17:57:02 +0400

    fix: replace id with real user' name in notification card

--- src/components/ui/card/notification-card/notification-card.tsx
+++ src/components/ui/card/notification-card/notification-card.tsx
@@ -5,6 +5,8 @@ import UserAvatar from '@ui/avatar/avatar'
 import FriendButton from '@src/components/friends/friend-button/friend-button'
 import { cn } from '@utils/utils'
 import { type NotificationType } from '@src/types/api'
+import { useAppSelector } from '@src/hooks/store-hooks'
+import { selectAllUsers } from '@src/store/reducers/friends/selectors'
 
 export interface RequestCardProps {
   img?: string
@@ -21,6 +23,8 @@ const text = {
 }
 
 const NotificationCard: FC<RequestCardProps> = ({ className, date, name, type }) => {
+  const user = useAppSelector(selectAllUsers).filter((user) => user.id === name)[0]
+
   return (
     <Card className={cn('block', className)}>
       <div className='flex items-center gap-[15px]'>
@@ -29,7 +33,7 @@ const NotificationCard: FC<RequestCardProps> = ({ className, date, name, type })
         <div>
           <h3 className='leading-6 text-[#55677D]'>
             <span className='font-bold text-liked-foreground flex flex-col lg:flex-row'>
-              {name}
+              {user?.firstName} {user?.lastName}
             </span>{' '}
             {text[type as keyof typeof text]}
           </h3>
