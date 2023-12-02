08cef9f019dd5e9148d7cf3ee71534f65ced7148
Dunissimo <SuperDen214@yandex.ru>
2023-12-02 17:43:34 +0400

    refactor: rewrite selectors, remove warnings

--- src/store/reducers/messenger/selectors.ts
+++ src/store/reducers/messenger/selectors.ts
@@ -1,13 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit'
 import { type RootState } from '@src/store/store'
 import { type IChat } from '@src/types/api'
 
-export const selectMessengerUserChats: (store: RootState) => IChat[] = ({ messenger }) =>
-  [...messenger.userChats].sort((a, b) => {
-    const aDate = new Date(a.newMessage?.createdAt || a.updatedAt)
-    const bDate = new Date(b.newMessage?.createdAt || b.updatedAt)
+//  Эта логика мемоизирует store и убирает ререндеры
+// было в документации в варнинге
+// https://redux.js.org/usage/deriving-data-selectors#optimizing-selectors-with-memoization
 
-    return bDate.getTime() - aDate.getTime()
-  })
+const userChatsSelector = ({ messenger }: RootState): IChat[] => messenger.userChats
+export const selectMessengerUserChats: (store: RootState) => IChat[] = createSelector(
+  [userChatsSelector],
+  (messages) =>
+    [...messages].sort((a, b) => {
+      const aDate = new Date(a.newMessage?.createdAt || a.updatedAt)
+      const bDate = new Date(b.newMessage?.createdAt || b.updatedAt)
+
+      return bDate.getTime() - aDate.getTime()
+    })
+)
 
 export const selectMessengerIsLoading: (store: RootState) => boolean = (store) =>
   store.friends.isLoading
--- src/store/reducers/notifications/selectors.ts
+++ src/store/reducers/notifications/selectors.ts
@@ -1,15 +1,23 @@
+import { createSelector } from '@reduxjs/toolkit'
 import { type RootState } from '@src/store/store'
 import { type INotification } from '@src/types/api'
 
-export const selectNotifications: (store: RootState) => INotification[] = ({
-  notifications
-}) =>
-  [...notifications.notifications].sort((a, b) => {
-    const aDate = new Date(a.createdAt || a.updatedAt)
-    const bDate = new Date(b.createdAt || b.updatedAt)
+//  Эта логика мемоизирует store и убирает ререндеры
+// было в документации в варнинге
+// https://redux.js.org/usage/deriving-data-selectors#optimizing-selectors-with-memoization
 
-    return bDate.getTime() - aDate.getTime()
-  })
+const userChatsSelector = ({ notifications }: RootState): INotification[] =>
+  notifications.notifications
+export const selectNotifications: (store: RootState) => INotification[] = createSelector(
+  [userChatsSelector],
+  (notifications) =>
+    [...notifications].sort((a, b) => {
+      const aDate = new Date(a?.createdAt || a.updatedAt)
+      const bDate = new Date(b?.createdAt || b.updatedAt)
+
+      return bDate.getTime() - aDate.getTime()
+    })
+)
 
 export const selectNotificationsIsLoading: (store: RootState) => boolean = (store) =>
   store.friends.isLoading
