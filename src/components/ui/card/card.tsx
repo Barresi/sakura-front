70b0ee1d3f99625245560fba4e4c4ac92d2ac5dd
Dunissimo <SuperDen214@yandex.ru>
2023-12-05 22:22:46 +0400

    fix: Баг в MessageCard, (нет padding) issue #43

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -10,7 +10,7 @@ const Card: FC<ICardProps> = ({ children, className }) => {
   return (
     <div
       className={cn(
-        'w-full bg-white dark:bg-grayBlue rounded-tl-[10px] border border-white dark:border-grayBlue border-b-smokyWhite dark:border-b-cadet hover:bg-ghostlyWhite dark:hover:bg-brownBlack px-0 usm:px-[15px] lg:px-[30px] py-[20px] hover:border',
+        'w-full bg-white dark:bg-grayBlue rounded-tl-[10px] border border-white dark:border-grayBlue border-b-smokyWhite dark:border-b-cadet hover:bg-ghostlyWhite dark:hover:bg-brownBlack px-[15px] lg:px-[30px] py-[20px] hover:border',
         className
       )}
     >
