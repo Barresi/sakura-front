81924a36a1c968b484a67333332c9dafe058be8b
Dunissimo <SuperDen214@yandex.ru>
2023-12-22 19:51:12 +0400

    fix: mobile nav layout on mobile

--- src/components/mobile-nav/mobile-nav.tsx
+++ src/components/mobile-nav/mobile-nav.tsx
@@ -14,10 +14,10 @@ const MobileNav: FC = () => {
     <div
       className={`${
         pathname.includes('/main/messenger/') && pathname.length > 16 ? 'hidden' : 'flex'
-      } w-full fixed bottom-0 left-0 flex items-center justify-between gap-[5px] lg:gap-[10px] bg-white dark:bg-grayBlue text-signalBlack dark:text-smokyWhite py-[10px] px-[10px] lg:px-[20px] rounded-tl-[10px] rounded-tr-[10px] z-[10000] border-t border-t-smokyWhite dark:border-t-cadet`}
+      } w-full fixed bottom-0 left-0 flex items-center justify-between lg:gap-[10px] bg-white dark:bg-grayBlue text-signalBlack dark:text-smokyWhite lg:px-[20px] rounded-tl-[10px] rounded-tr-[10px] z-[10000] border-t border-t-smokyWhite dark:border-t-cadet`}
     >
       <NavButton
-        className='w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]'
+        className='w-full h-full flex-col px-0 justify-center items-center text-center text-[12px] md:text-[14px]'
         icon='user'
         to='profile'
       >
@@ -25,7 +25,7 @@ const MobileNav: FC = () => {
       </NavButton>
 
       <NavButton
-        className='w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]'
+        className='w-full h-full flex-col px-0 justify-center items-center text-center text-[12px] md:text-[14px]'
         icon='news'
         to='feed'
       >
@@ -33,7 +33,7 @@ const MobileNav: FC = () => {
       </NavButton>
 
       <NavButton
-        className='w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]'
+        className='w-full h-full flex-col px-0 justify-center items-center text-center text-[12px] md:text-[14px]'
         icon='message'
         to='messenger'
         badge={totalUnreadMessages}
@@ -42,7 +42,7 @@ const MobileNav: FC = () => {
       </NavButton>
 
       <NavButton
-        className='w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]'
+        className='w-full h-full flex-col px-0 justify-center items-center text-center text-[12px] md:text-[14px]'
         icon='friends'
         to='friends'
         badge={received.length}
@@ -51,7 +51,7 @@ const MobileNav: FC = () => {
       </NavButton>
 
       <NavButton
-        className='w-full flex-col justify-center items-center text-center px-[5px] first-letter:lg:px-[15px] py-[30px] gap-[5px] text-[12px] md:text-[14px]'
+        className='w-full h-full flex-col px-0 justify-center items-center text-center text-[12px] md:text-[14px]'
         icon='photos'
         to='photos'
       >
