bdfa4decc95858848512016a5ea2b2de44dd5c9a
Barresi <Arhcer.php@gmail.com>
2024-01-10 01:37:26 +0700

    fix: routing in app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,6 @@
-import LoginPage from '@src/pages/page-login/login'
-import RegistrationPage from '@src/pages/page-registration/registration'
 import { type FC, useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import ProtectedRouteElement from './protected-route-element/protected-route-element'
-import MainPage from '@src/pages/page-main/main'
-import NotFoundPage from '@src/pages/page-not-found/not-found-page'
-import FriendsPage from '@src/pages/page-friends/ui/friends-page/friends-page'
 import { useAppDispatch } from '@src/shared/lib/hooks/store-hooks'
 import { userInfoThunk } from '@src/app/store/reducers/profileInfo/async-thunks'
-import MessengerPage from '@src/pages/page-messenger/messenger'
-import Chat from '../widgets/chat/ui/chat'
 import Toaster from '../widgets/toaster/ui/toaster/toaster'
 import { useSocket } from '@src/app/providers/socket-context/useSocket'
 import { useToast } from '../widgets/toaster/lib/use-toast'
@@ -27,6 +18,7 @@ import {
 import { getUserChatsThunk } from '@src/app/store/reducers/messenger/async-thunks'
 import { getUserNotificationsThunk } from '@src/app/store/reducers/notifications/async-thunks'
 import { NotificationTypeEnum } from '@src/shared/lib/types/api'
+import AppRouter from './router/ui/app-router'
 
 interface payloadNtfFnc {
   friendId: string
@@ -110,38 +102,7 @@ const App: FC = () => {
     // relative нужен для компонента Sheet
     <div className='relative'>
       <Toaster />
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <ProtectedRouteElement protectedPageType='auth' element={<LoginPage />} />
-          }
-        />
-        <Route
-          path='/registration'
-          element={
-            <ProtectedRouteElement
-              protectedPageType='auth'
-              element={<RegistrationPage />}
-            />
-          }
-        />
-
-        <Route
-          path='/main'
-          element={
-            <ProtectedRouteElement protectedPageType='main' element={<MainPage />} />
-          }
-        >
-          <Route path='messenger' element={<MessengerPage />}>
-            <Route path=':id' element={<Chat />} />
-          </Route>
-          <Route path='friends' element={<FriendsPage />} />
-          <Route path='*' element={<NotFoundPage type='inside' />} />
-        </Route>
-
-        <Route path='*' element={<NotFoundPage type='outside' />} />
-      </Routes>
+      <AppRouter />
     </div>
   )
 }
---
+++ src/app/router/ui/app-router.tsx
@@ -0,0 +1,51 @@
+import ProtectedRouteElement from '@src/app/router/ui/protected-route-element'
+import FriendsPage from '@src/pages/page-friends/ui/friends-page/friends-page'
+import LoginPage from '@src/pages/page-login/login'
+import MainPage from '@src/pages/page-main/main'
+import MessengerPage from '@src/pages/page-messenger/messenger'
+import NotFoundPage from '@src/pages/page-not-found/not-found-page'
+import RegistrationPage from '@src/pages/page-registration/registration'
+import Chat from '@src/widgets/chat/ui/chat'
+import { type FC, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const AppRouter: FC = () => {
+  return (
+    <Suspense fallback={<div>1</div>}>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRouteElement protectedPageType='auth' element={<LoginPage />} />
+          }
+        />
+        <Route
+          path='/registration'
+          element={
+            <ProtectedRouteElement
+              protectedPageType='auth'
+              element={<RegistrationPage />}
+            />
+          }
+        />
+
+        <Route
+          path='/main'
+          element={
+            <ProtectedRouteElement protectedPageType='main' element={<MainPage />} />
+          }
+        >
+          <Route path='messenger' element={<MessengerPage />}>
+            <Route path=':id' element={<Chat />} />
+          </Route>
+          <Route path='friends' element={<FriendsPage />} />
+          <Route path='*' element={<NotFoundPage type='inside' />} />
+        </Route>
+
+        <Route path='*' element={<NotFoundPage type='outside' />} />
+      </Routes>
+    </Suspense>
+  )
+}
+
+export default AppRouter
