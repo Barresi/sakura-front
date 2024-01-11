71df23767a630c9bbc28019aafa23b22bf24b49a
Barresi <Arhcer.php@gmail.com>
2024-01-12 02:45:59 +0700

    fix: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
+import App from '@app/app'
+import { SocketProvider } from '@app/providers/socket-context/ui/socket-provider'
+import { ThemeProvider } from '@app/providers/theme-context/ui/theme-provider'
+import { store } from '@app/store/store'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-
-import App from '@src/app/app'
-import ThemeProvider from '@src/app/providers/theme-context/ui/theme-provider'
-import { store } from '@src/app/store/store'
-import SocketProvider from './app/providers/socket-context/ui/socket-provider'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
