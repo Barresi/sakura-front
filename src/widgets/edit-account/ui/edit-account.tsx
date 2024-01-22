b6114c392a5caaea476fce651affad0097300c91
Barresi <Arhcer.php@gmail.com>
2024-01-23 02:12:33 +0700

    fix: select male or female

--- src/widgets/edit-account/ui/edit-account.tsx
+++ src/widgets/edit-account/ui/edit-account.tsx
@@ -1,6 +1,68 @@
+import { Button } from '@shared/ui/button'
+import { Input } from '@shared/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@shared/ui/select'
+import { Textarea } from '@shared/ui/textarea'
 import { type FC } from 'react'
 
 const EditAccount: FC = () => {
-  return <div>2</div>
+  return (
+    <div className='flex flex-col gap-5'>
+      <h1 className='text-2xl'>Аккаунт</h1>
+      <div className='relative'>
+        <div className='flex flex-col md:flex-row md:gap-5 justify-between'>
+          <div className='w-[100%] flex flex-col gap-1'>
+            <h3 className='text-sm'>Никнейм</h3>
+            <Input />
+          </div>
+          <div className='w-[100%] flex flex-col gap-1'>
+            <h3 className='text-sm'>Родной город</h3>
+            <Input />
+          </div>
+        </div>
+        <div className='flex flex-col md:flex-row md:gap-5 justify-between'>
+          <div className='w-[100%] flex flex-col gap-1'>
+            <h3>Имя</h3>
+            <Input />
+          </div>
+          <div className='w-[100%] flex flex-col gap-1'>
+            <h3>Фамилия</h3>
+            <Input />
+          </div>
+        </div>
+        <div className='flex flex-col md:flex-row md:gap-5 justify-between'>
+          <div className='w-[100%] flex flex-col gap-1'>
+            <h3>День рождения</h3>
+            <Input />
+          </div>
+          <div className='w-[100%] flex flex-col gap-1'>
+            <h3>Пол</h3>
+            <Select defaultValue='male'>
+              <SelectTrigger className='mb-6 rounded-[6px]'>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent className='border-smokyWhite dark:border-cadet rounded-[6px] w-selectWidth'>
+                <SelectItem value='male'>Мужской</SelectItem>
+                <SelectItem value='female'>Женский</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className='w-[100%] flex flex-col gap-1'>
+          <h3>Краткая информация</h3>
+          <Textarea />
+        </div>
+      </div>
+      <div className='flex flex-col sm:flex-row gap-3 md:w-[480px] md:self-end'>
+        <Button variant='secondary'>Отмена</Button>
+        <Button variant='default'>Сохранить</Button>
+      </div>
+    </div>
+  )
 }
 export { EditAccount }
--- src/widgets/edit-security/ui/edit-security.tsx
+++ src/widgets/edit-security/ui/edit-security.tsx
@@ -1,6 +1,12 @@
 import { type FC } from 'react'
 
 const EditSecurity: FC = () => {
-  return <div>1</div>
+  return (
+    <div>
+      <h1 className='text-2xl'>Безопасность</h1>
+      <div></div>
+      <div></div>
+    </div>
+  )
 }
 export { EditSecurity }
