200b88d97be381db5913cc6f0d4eda7090c4e5fd
Barresi <Arhcer.php@gmail.com>
2024-02-14 22:19:35 +0700

    fix: default theme

--- src/app/providers/theme-context/ui/theme-provider.tsx
+++ src/app/providers/theme-context/ui/theme-provider.tsx
@@ -1,21 +1,20 @@
 import { useEffect, useState, type FC } from 'react'
 import {
   LOCAL_STORAGE_THEME_KEY,
-  Theme,
-  ThemeProviderContext
+  ThemeProviderContext,
+  type Theme
 } from '../lib/theme-context'
 
 interface IThemeProviderProps {
   children: React.ReactNode
-  defaultTheme?: Theme
+  defaultTheme: Theme
   storageKey?: string
 }
 
-const defaultTheme =
-  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
-
-const ThemeProvider: FC<IThemeProviderProps> = ({ children }) => {
-  const [theme, setTheme] = useState<Theme>(defaultTheme)
+const ThemeProvider: FC<IThemeProviderProps> = ({ children, defaultTheme }) => {
+  const [theme, setTheme] = useState<Theme>(
+    (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || defaultTheme
+  )
   useEffect(() => {
     const root = window.document.documentElement
 
