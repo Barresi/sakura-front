3128506399b6f83d8a029801ea8fb7ef3b3c1d0a
Barresi <Arhcer.php@gmail.com>
2024-02-14 21:48:52 +0700

    fix: deleting friend #105

--- src/features/button-friend-delete/ui/button-friend-delete.tsx
+++ src/features/button-friend-delete/ui/button-friend-delete.tsx
@@ -2,6 +2,16 @@ import { getFriendsThunk } from '@app/store/reducers/friends/async-thunks'
 import { deleteFriend } from '@shared/api/friends/friends'
 import { useAppDispatch } from '@shared/lib/hooks/store-hooks'
 import { Button } from '@shared/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@shared/ui/dialog'
 import { type FC } from 'react'
 
 interface ButtonFriendDeleteProps {
@@ -20,28 +30,48 @@ const ButtonFriendDelete: FC<ButtonFriendDeleteProps> = ({
 
     dispatch(getFriendsThunk())
   }
-  if (isMobile) {
-    return (
-      <Button
-        icon={'deleteFriend'}
-        variant='secondary'
-        className='w-[49%] whitespace-nowrap hover:bg-secondary-hover'
-        onClick={async () => {
-          await deleteFriendHandler()
-        }}
-      />
-    )
-  }
+
   return (
-    <Button
-      variant='secondary'
-      className={className}
-      onClick={async () => {
-        await deleteFriendHandler()
-      }}
-    >
-      Удалить из друзей
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        {isMobile ? (
+          <Button
+            icon={'deleteFriend'}
+            variant='secondary'
+            className='w-[49%] whitespace-nowrap hover:bg-secondary-hover'
+          />
+        ) : (
+          <Button variant='secondary' className={className}>
+            Удалить из друзей
+          </Button>
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        {/* @ts-expect-error не рабочие пропсы у DialogHeader */}
+        <DialogHeader>
+          <DialogTitle>Удалить из друзей</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          Это действие не может быть отменено. Вы уверены, что хотите удалить этого юзера
+          из списка ваших друзей?
+        </DialogDescription>
+
+        {/* @ts-expect-error не рабочие пропсы у DialogFooter */}
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant='secondary'>Отменить</Button>
+          </DialogClose>
+          <Button
+            variant='default'
+            onClick={async () => {
+              await deleteFriendHandler()
+            }}
+          >
+            Удалить
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 export { ButtonFriendDelete }
