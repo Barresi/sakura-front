75640ab4cd97d54fad6610d7dfff7bacddb242e2
Barresi <Arhcer.php@gmail.com>
2024-02-26 21:01:06 +0700

    fix: spacing in dialog #123

--- src/shared/ui/dialog.tsx
+++ src/shared/ui/dialog.tsx
@@ -35,12 +35,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 p-6 bg-white dark:bg-grayBlue shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg',
+        'fixed left-[50%] top-[50%] z-50 w-full max-w-[552px] translate-x-[-50%] translate-y-[-50%] shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
         className
       )}
       {...props}
     >
-      {children}
+      <div className='p-6 mx-5 bg-white dark:bg-grayBlue rounded-lg grid gap-4'>
+        {children}
+      </div>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
