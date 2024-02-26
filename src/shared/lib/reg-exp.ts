3e6e7738ed0b7ede4658124d1573ae9406800d62
Barresi <Arhcer.php@gmail.com>
2024-02-26 21:23:14 +0700

    fix: password reg-exp

--- src/shared/lib/reg-exp.ts
+++ src/shared/lib/reg-exp.ts
@@ -1,4 +1,4 @@
-export const passwordRegExp = /^[a-zA-Z!@#-$%^&*\d]+$/
+export const passwordRegExp = /^[a-zA-Z!@#-_=+$%()/.,`^&*\d]+$/
 export const emailRegExp = /^(?=.{1,256}$)[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
 export const usernameRegExp = /^[a-zA-Z0-9_]*@{1}[a-zA-Z0-9_]+$/
 export const nameRegExp = /^[а-яА-Яa-zA-Z]+$/u
