ae86305159ee4acdd39c853be1e6e13c2ff5ddc3
Barresi <Arhcer.php@gmail.com>
2024-04-22 23:43:05 +0700

    fix: color settings (2)

--- src/widgets/mobile-nav/ui/mobile-nav.tsx
+++ src/widgets/mobile-nav/ui/mobile-nav.tsx
@@ -50,7 +50,7 @@ const MobileNav: FC = () => {
 
       <ButtonNav
         className='w-full h-full flex-col px-0 justify-center items-center text-center text-[12px] md:text-[14px]'
-        icon='settings'
+        icon='settingsRed'
         to={AppRoutes.SETTINGS}
       >
         Настройки
