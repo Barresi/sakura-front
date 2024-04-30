0f5f11d76d9d8545cfbf1dbcba27f9ae5b7606a5
Barresi <Arhcer.php@gmail.com>
2024-04-30 23:19:47 +0700

    fix: imports news post

--- src/pages/page-profile/ui/page-profile.tsx
+++ src/pages/page-profile/ui/page-profile.tsx
@@ -1,7 +1,7 @@
 import { selectAllUsers } from '@app/store/reducers/friends/selectors'
 import { selectAllPosts } from '@app/store/reducers/news/selectors'
 import { selectUser } from '@app/store/reducers/profileInfo/selectors'
-import { PostNews } from '@entities/post-news/ui/post-news'
+import { PostNews } from '@entities/post-news'
 import { ButtonLikePost } from '@features/button-like-post'
 import { InputCreatePost } from '@features/input-create-post'
 import { useAppSelector } from '@shared/lib/hooks/store-hooks'
