91624f5f089a24e78f63b46172713ddb7e32f444
Barresi <Arhcer.php@gmail.com>
2024-07-05 21:44:59 +0700

    fix: banner height

--- src/pages/page-profile/ui/page-profile.tsx
+++ src/pages/page-profile/ui/page-profile.tsx
@@ -33,7 +33,7 @@ const PageProfile: FC = () => {
         <BlockProfile user={currentUser} isMyProfile={isMyProfile} friends={friends} />
         <div className='w-full xxl:w-2/3 rounded-[10px] flex flex-col gap-[20px] xl:gap-[30px]'>
           <Banner
-            className='h-[180px] sm:h-[295px] lg:h-[337px]'
+            className='h-[180px] sm:h-[295px] lg:h-full'
             src={currentUser?.banner || null}
           />
           {/* mobile user info */}
