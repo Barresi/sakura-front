d64bf4df35ee1dd6850aa336b271402218947cda
Barresi <Arhcer.php@gmail.com>
2024-07-06 16:39:16 +0700

    fix: hover in settings

--- src/widgets/edit-account/ui/avatar-change/avatar-change.tsx
+++ src/widgets/edit-account/ui/avatar-change/avatar-change.tsx
@@ -38,13 +38,13 @@ const AvatarChange: FC = () => {
         }, [previewUrl, user?.avatar])
         return (
           <div
-            className='cursor-pointer relative mt-[50px] h-[100%] w-[100%] usm:absolute usm:mt-0 usm:left-[20px] usm:bottom-[20px] usm:w-[150px] usm:h-[150px] lg:left-[30px] lg:bottom-[30px] xxl:left-[20px] rounded-full usm:border-[4px] dark:border-grayBlue'
+            className='cursor-pointer relative mt-[50px] h-[100%] w-[100%] usm:absolute usm:mt-0 usm:left-[20px] usm:bottom-[20px] usm:w-[150px] usm:h-[150px] lg:left-[30px] lg:bottom-[30px] xxl:left-[20px] rounded-full usm:border-[4px] dark:border-grayBlue group'
             onClick={() =>
               (document.getElementById('avatarRef') as HTMLInputElement | null)?.click()
             }
           >
             {/* Todo сделать затемнение только при ховере */}
-            <div className='absolute left-0 right-0 top-0 bottom-0 rounded-full flex justify-center items-center'>
+            <div className='absolute left-0 right-0 top-0 bottom-0 rounded-full flex lg:hidden justify-center items-center group-hover:flex'>
               <span className='z-[3] text-md usm:text-sm font-medium text-white'>
                 Изменить фото
               </span>
