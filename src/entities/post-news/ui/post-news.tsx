4d487a2b91e74777787c322bfbef448515565079
Barresi <Arhcer.php@gmail.com>
2024-07-11 02:35:46 +0700

    fix: height (3)

--- src/entities/post-news/ui/post-news.tsx
+++ src/entities/post-news/ui/post-news.tsx
@@ -49,7 +49,7 @@ const PostNews: FC<IPostNewsProps> = ({ post, buttonLike, buttonDelete }) => {
             <CarouselItem key={ind}>
               <img
                 src={urlOnBackend + picture}
-                className='object-cover w-full h-full rounded-[10px] h-[400px]'
+                className='object-cover w-full h-full rounded-[10px] max-h-[400px] min-h-[150px]'
               />
             </CarouselItem>
           ))}
