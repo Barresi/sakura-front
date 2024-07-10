1b765d33e1cff97579ef040e163e260bc5a57418
Barresi <Arhcer.php@gmail.com>
2024-07-11 03:38:14 +0700

    fix: editAccount func

--- src/entities/post-news/ui/post-news.tsx
+++ src/entities/post-news/ui/post-news.tsx
@@ -1,12 +1,12 @@
+import { useWindowSize } from '@shared/lib/hooks/useWindowSize'
 import { parseDateToMonth, parseDateToTime } from '@shared/lib/parse-date'
 import { type IPost } from '@shared/lib/types/api'
+import { CarouselItem, CarouselWithPoints } from '@shared/ui/carousel'
 import { LinkName } from '@shared/ui/link-name'
 import { UserAvatar } from '@shared/ui/user-avatar'
 import { useState, type FC, type ReactNode } from 'react'
 
 import eye from '@assets/ui/Eye.svg'
-import { useWindowSize } from '@shared/lib/hooks/useWindowSize'
-import { CarouselItem, CarouselWithPoints } from '@shared/ui/carousel'
 
 interface IPostNewsProps {
   post: IPost | undefined
--- src/shared/lib/remove-null-properties.ts
+++ src/shared/lib/remove-null-properties.ts
@@ -2,7 +2,8 @@ type argObject = Record<string, any>
 
 export const removeNullProperties = (obj: argObject): argObject => {
   for (const prop in obj) {
-    if (obj[prop] === null || obj[prop] === '') delete obj[prop]
+    if (obj[prop] === null || obj[prop] === '' || obj[prop] === undefined)
+      delete obj[prop]
   }
   return obj
 }
